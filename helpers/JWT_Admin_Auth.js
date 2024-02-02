027fb22833dc7292f1de6f67173a9dcdb7a2c095
Sherif Shaaban <sherif.shaaban@your-project.ch>
2024-02-02 22:41:24 +0200

    Updated the user auth for the users and admins

---
+++ helpers/JWT_Admin_Auth.js
@@ -0,0 +1,16 @@
+const expressJwt = require("express-jwt");
+
+const adminAuth = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["HS256"],
+  isRevoked: isRevoked,
+});
+
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    return done(null, true);
+  }
+  return done();
+}
+
+module.exports = adminAuth;
