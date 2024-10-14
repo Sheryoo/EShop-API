67a41ffd0a867db92c22387bdbe1f2caaf09aa5e
sheryoo <sherifzayan5@gmail.com>
2024-10-14 18:14:26 +0300

    Feat: add rate limiter

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import { config } from "dotenv";
 import cors from "cors";
+import path from "path";
 
 // Load Environment Variables
 config();
@@ -10,6 +11,7 @@ config();
 import normalUserRoutes from "./routes/normal-user";
 import adminRoutes from "./routes/admin";
 import errorHandler from "./helpers/error-handler";
+import apiLimiter from "./helpers/rate-limiter";
 
 const API_URI = process.env.API_URI;
 const ADMIN_URI = process.env.ADMIN_URI;
@@ -23,6 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
+app.use(`${API_URI}`, apiLimiter);
 
 // Normal User Routes
 app.use(`${API_URI}/products`, normalUserRoutes.productRouter);
@@ -36,6 +39,9 @@ app.use(`${ADMIN_URI}/users`, adminRoutes.userRouter);
 app.use(`${ADMIN_URI}/categories`, adminRoutes.categoryRouter);
 app.use(`${ADMIN_URI}/orders`, adminRoutes.orderRouter);
 app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // Error Handler for not found routes
 app.use(errorHandler);
---
+++ helpers/rate-limiter.ts
@@ -0,0 +1,15 @@
+import rateLimit from "express-rate-limit";
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 1000 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: {
+    status: false,
+    message: "Too many requests, please try again later.",
+    data: null,
+  },
+});
+
+export default apiLimiter;
